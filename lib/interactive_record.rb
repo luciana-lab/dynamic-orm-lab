e46eda8d285fdb9a4782c9adcb00162c2da13fb3
Luciana Sugeta <lulusugeta@gmail.com>
2021-04-28 18:02:30 -0400

    commit tree 67017f60894e364bbb4588fd3b5a4cdd50c311a1

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,70 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+    #creates a downcased, plural table name based on the Class name
+    def self.table_name
+        self.to_s.downcase.pluralize
+    end
+
+    #returns an array of SQL column names
+    def self.column_names
+        DB[:conn].results_as_hash = true
+        sql = "pragma table_info ('#{table_name}')"
+
+        table_info = DB[:conn].execute(sql)
+        column_names = []
+        table_info.each do |row|
+            column_names << row["name"]
+        end
+    column_names.compact
+    end
+
+     #creates an new instance of a student
+    #creates a new student with attributes
+    def initialize(options={})
+        options.each do |column, value|
+            self.send("#{column}=", value)
+        end
+    end
+
+    #return the table name when called on an instance of Student
+    def table_name_for_insert
+        self.class.table_name
+    end
+
+    #return the column names when called on an instance of Student
+    #does not include an id column
+    def col_names_for_insert
+        self.class.column_names.delete_if {|col_name| col_name == "id"}.join(", ")
+    end
+
+    #formats the column names to be used in a SQL statement
+    def values_for_insert
+        values = []
+        self.class.column_names.each do |column|
+            values << "'#{send(column)}'" unless send(column).nil?
+        end
+    values.join(", ")
+    end
+
+    #saves the student to the db
+    #sets the student's id
+    def save
+        sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+        DB[:conn].execute(sql)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+
+    def self.find_by_name(name)
+        sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+        DB[:conn].execute(sql)
+    end
+
+    def self.find_by(attribute_hash)
+        value = attribute_hash.values.first
+        formatted_value = value.class == Fixnum ? value : "'#{value}'"
+        sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+        DB[:conn].execute(sql)
+    end
+
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,14 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
+#inherits from InteractiveRecord class
 class Student < InteractiveRecord
+   
+    #creates attr_accessors for each column name
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+    end
 
 end
